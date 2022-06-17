ac2f569826ecd950548dc533c09e651932713fd6
dahciP <104251707+dahciP@users.noreply.github.com>
2022-06-17 08:56:00 +0530

    Delete tkinter directory

--- tkinter/message.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk
-master = tk.Tk()
-sm = "Welcome to SM INFOTECH Python Program.\n\n\n(By SM Classes)"
-msg = tk.Message(master, text = sm)
-msg.config(bg='lightgreen',font=('times',24,'italic'))
-msg.pack()
-tk.mainloop()
--- tkinter/text.py
+++
@@ -1,5 +0,0 @@
-import tkinter as tk
-root = tk.Tk()
-w = tk.Label(root, text="Welcome to SM InfoTech!")
-w.pack()
-root.mainloop()
